7135c18c7aa204b0a0d20671f6e368e866f0746c
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-21 19:23:26 -0600

    agregando src/test/java/pages/tc13Page.java

---
+++ src/test/java/pages/tc13Page.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc13Page {
+
+    WebDriver driver;
+
+    public tc13Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void performAction13() {
+        // Logic to perform action 13
+    }
+
+    public void verifyResultadoEsperado13() {
+        // Logic to verify resultado esperado 13
+    }
+}
